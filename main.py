c1a7aecbd2ef5cb80e70b1d6ecb3eb6f5bb15f89
Prince Sonani <princesonani9112@gmail.com>
2023-08-20 00:46:45 -0500

    Test

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# What is this program
+
+
+
+# I will create a personal todo list-type application. Entitled, "What's Next?"
+# Levels of Importance
+
+# Log-in System
+
+# To-do List
+    # Priorities
+    # Queue
+    # Reminders
+    # etc.
+
+# py -m pip install Django 
+# ^^ Install django on xps
+
+print("Hallo World")
+
